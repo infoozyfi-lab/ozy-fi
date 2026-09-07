97c6801dfdfe5feba4318b34c59e227e38b28357
infoozyfi-lab <info.ozyfi@gmail.com>
2026-09-08 00:47:22 +0300

    Add files via upload

---
+++ components/MenuPageClient.js
@@ -0,0 +1,30 @@
+'use client';
+
+import { StoreProvider } from '@/context/StoreContext';
+import Header from '@/components/Header';
+import MenuSection from '@/components/MenuSection';
+import Footer from '@/components/Footer';
+import ProductPage from '@/components/ProductPage';
+import BundleModal from '@/components/BundleModal';
+import DrinkUpsellModal from '@/components/DrinkUpsellModal';
+import CheckoutModal from '@/components/CheckoutModal';
+import ConfirmModal from '@/components/ConfirmModal';
+import OrderBar from '@/components/OrderBar';
+
+export default function MenuPageClient() {
+  return (
+    <StoreProvider>
+      <div id="top" />
+      <Header />
+      <MenuSection />
+      <Footer />
+
+      <ProductPage />
+      <BundleModal />
+      <DrinkUpsellModal />
+      <CheckoutModal />
+      <ConfirmModal />
+      <OrderBar />
+    </StoreProvider>
+  );
+}
