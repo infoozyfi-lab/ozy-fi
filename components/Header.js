53db98ae6adfe44cf4cb34bc219b214c84f7dc15
infoozyfi-lab <info.ozyfi@gmail.com>
2026-09-08 09:13:32 +0300

    Add files via upload

--- components/Header.js
+++ components/Header.js
@@ -7,7 +7,7 @@ import { useStore } from '@/context/StoreContext';
 
 export default function Header() {
   const [mobileOpen, setMobileOpen] = useState(false);
-  const { goToCheckout, cart } = useStore();
+  const { goToCheckoutDirect, cart } = useStore();
   const pathname = usePathname();
   const itemCount = cart.reduce((sum, line) => sum + line.qty, 0);
 
@@ -53,7 +53,7 @@ export default function Header() {
           <li><a href="/track">Track order</a></li>
         </ul>
         <div className="nav-order">
-          <button className="cart-icon-btn" type="button" aria-label="Cart" onClick={goToCheckout}>
+          <button className="cart-icon-btn" type="button" aria-label="Cart" onClick={goToCheckoutDirect}>
             <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
               <path
                 d="M7 8V6.5C7 4.01472 9.01472 2 11.5 2H12.5C14.9853 2 17 4.01472 17 6.5V8"
