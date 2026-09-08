f4da0859b20ed047859e3bf911352c1b4432f08c
infoozyfi-lab <info.ozyfi@gmail.com>
2026-09-08 10:35:30 +0300

    Add files via upload

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
 import './globals.css';
+import CookieBanner from '@/components/CookieBanner';
 
 export const metadata = {
   metadataBase: new URL('https://ozy.fi'),
@@ -24,7 +25,10 @@ export default function RootLayout({ children }) {
           rel="stylesheet"
         />
       </head>
-      <body>{children}</body>
+      <body>
+        {children}
+        <CookieBanner />
+      </body>
     </html>
   );
 }
